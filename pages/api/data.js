ce7182d4439adbfc2fc6e332caa2700fe7b1cd40
nicola genesin <nicolagenesin@yahoo.it>
2021-12-15 02:09:41 +0100

    Revert "stale-while-revalidate test"
    This reverts commit 6d7f848378d1e1b8a8d95f96ab34f1e0e90bbc9e.

--- pages/api/data.js
+++ pages/api/data.js
@@ -24,6 +24,9 @@ const getResults = async () => {
 
         if (rawData[0] !== '') {
             currentKey = rawData[0]
+
+            console.log(currentKey)
+
             results[currentKey] = [row._rawData]
         } else {
             results[currentKey].push(row._rawData)
@@ -34,8 +37,6 @@ const getResults = async () => {
 }
 
 const handler = async (req, res) => {
-    console.log('Endpoint called')
-
     res.status(200).json(await getResults())
 }
 
--- pages/nofoodaftermidnight.js
+++ pages/nofoodaftermidnight.js
@@ -176,22 +176,4 @@ const App = () => {
     )
 }
 
-// This value is considered fresh for ten seconds (s-maxage=10).
-// If a request is repeated within the next 60 seconds, the previously
-// cached value will still be fresh. If the request is repeated before 59 seconds,
-// the cached value will be stale but still render (stale-while-revalidate=59).
-//
-// In the background, a revalidation request will be made to populate the cache
-// with a fresh value. If you refresh the page, you will see the new value.
-export async function getServerSideProps({ req, res }) {
-    res.setHeader(
-        'Cache-Control',
-        'public, s-maxage=60, stale-while-revalidate=59'
-    )
-
-    return {
-        props: {},
-    }
-}
-
 export default App
