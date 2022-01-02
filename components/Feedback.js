9490da131253673ce7f39b7aa91035078452870c
nicola genesin <nicolagenesin@yahoo.it>
2022-01-02 18:34:37 +0100

    Make feedback more prominent

--- components/Feedback.js
+++ components/Feedback.js
@@ -5,37 +5,35 @@ import { IoMdClose } from "react-icons/io";
 
 const Feedback = () => {
   const [isButtonClicked, setIsButtonClicked] = useState(false);
-  const [isSent, setIsSent] = useState(false);
-  const [content, setContent] = useState({});
 
   return (
     <Box
-      h="auto"
-      w="auto"
-      maxW="350px"
       position="fixed"
-      bottom="0"
+      top="50%"
       right="0"
+      margin="0"
       boxShadow="md"
-      borderRadius="5px 0 0 0"
-      bg="purple"
+      borderRadius="5px 5px 0 0"
       color="#ebece8"
       fontWeight="bold"
-      p="4px 4px"
     >
       {!isButtonClicked ? (
         <Text
+          p="4px 6px"
+          bg="purple"
+          transform="translateX(30%) rotate(-90deg)"
           fontSize="xs"
           cursor={"pointer"}
           onClick={() => {
             setIsButtonClicked(!isButtonClicked);
           }}
         >
-          👀
+          👀 Feedback
         </Text>
       ) : (
-        <Box p="6px 4px">
-          <HStack>
+        <Box p="12px" bg="purple">
+          <HStack mb="16px">
+            <Text fontWeight="semibold">Bugs? 🐛 Feedback? Ideas? 👇</Text>
             <Spacer />
             <Box
               rounded="md"
@@ -47,12 +45,10 @@ const Feedback = () => {
               <IoMdClose />
             </Box>
           </HStack>
-          <Text fontWeight="semibold" mb="8px">
-            Bugs? 🐛 Feedback? 👇
-          </Text>
           <Text fontSize="xs">
-            If you want a feature, found a bug or have feedback, write me on
-            Discord. I'll build the feature / fix the bug for you!
+            If you want a feature, found a bug or have feedback,
+            <br />
+            write me on Discord. I'll build the feature / fix the bug for you!
           </Text>
           <Link href="https://discord.gg/ZmK8PdWPV9">
             <Center rounded="md" bg={"blackAlpha.500"} mt="8px" py="4px">
