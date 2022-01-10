c6de7c917e8842d494968e1c2c9ad0487ef3cbf8
nicola genesin <nicolagenesin@yahoo.it>
2022-01-10 14:59:07 +0100

    Complete chart view and some general refactoring

--- components/Chart.js
+++ components/Chart.js
@@ -1,10 +1,238 @@
 import React from "react";
 import Chart from "chart.js/auto";
 import ChartDataLabels from "chartjs-plugin-datalabels";
-import { Box, Button } from "@chakra-ui/react";
+import annotationPlugin from "chartjs-plugin-annotation";
+
+import { Box, Center } from "@chakra-ui/react";
+import getColor from "../utils/getColor";
+
+Chart.defaults.font.size = 14;
+Chart.defaults.font.weight = "bold";
+Chart.defaults.font.family = "Montserrat";
+
+console.log(Chart.defaults.font);
 
 Chart.register(ChartDataLabels);
+Chart.register(annotationPlugin);
 const defaultLegendClickHandler = Chart.defaults.plugins.legend.onClick;
+
+const lineLabelBackgroundColour = "green";
+const annotations = [];
+
+annotations.push({
+  type: "box",
+  backgroundColor: "transparent",
+  borderWidth: 0,
+  label: {
+    drawTime: "beforeDraw",
+    enabled: true,
+    color: "rgba(50, 50, 50, 0.2)",
+    content: "NoFAM",
+    font: {
+      size: (ctx) => ctx.chart.chartArea.height / 2,
+    },
+    position: "center",
+  },
+});
+
+for (let index = 1; index < 7; index++) {
+  annotations.push({
+    type: "line",
+    scaleID: "y2",
+    borderWidth: 1,
+    borderColor: "#383932",
+    value: index * 10,
+    label: {
+      rotation: "auto",
+      position: "end",
+      content: `CLASS ${index}`,
+      enabled: true,
+      backgroundColor: lineLabelBackgroundColour,
+    },
+    drawTime: "beforeDraw",
+  });
+}
+
+annotations.push({
+  type: "line",
+  scaleID: "x",
+  borderWidth: 1,
+  borderColor: "green",
+  value: 35,
+  borderDash: [8, 8],
+  label: {
+    rotation: "auto",
+    content: "HEAD",
+    position: "start",
+    enabled: true,
+    backgroundColor: lineLabelBackgroundColour,
+  },
+  drawTime: "beforeDraw",
+});
+
+annotations.push({
+  type: "line",
+  scaleID: "x",
+  borderWidth: 1,
+  borderColor: "green",
+  value: 75,
+  borderDash: [8, 8],
+  label: {
+    rotation: "auto",
+    content: "CHEST",
+    position: "start",
+    enabled: true,
+    backgroundColor: lineLabelBackgroundColour,
+  },
+  drawTime: "beforeDraw",
+});
+
+const getOrCreateTooltip = (chart) => {
+  let tooltipEl = chart.canvas.parentNode.querySelector("div");
+
+  if (!tooltipEl) {
+    tooltipEl = document.createElement("div");
+    tooltipEl.style.background = "rgba(0, 0, 0, 0.7)";
+    tooltipEl.style.borderRadius = "3px";
+    tooltipEl.style.color = "white";
+    tooltipEl.style.opacity = 1;
+    tooltipEl.style.pointerEvents = "none";
+    tooltipEl.style.position = "absolute";
+    tooltipEl.style.transform = "translate(-50%, 0)";
+    tooltipEl.style.transition = "all .1s ease";
+
+    const table = document.createElement("table");
+    table.style.margin = "0px";
+
+    tooltipEl.appendChild(table);
+    chart.canvas.parentNode.appendChild(tooltipEl);
+  }
+
+  return tooltipEl;
+};
+
+const externalTooltipHandler = (context) => {
+  // Tooltip Element
+  const { chart, tooltip } = context;
+  const tooltipEl = getOrCreateTooltip(chart);
+
+  // Hide if no tooltip
+  if (tooltip.opacity === 0) {
+    tooltipEl.style.opacity = 0;
+    return;
+  }
+
+  const ammo = context.tooltip.dataPoints[0].raw;
+
+  const tableHead = document.createElement("thead");
+
+  const tr = document.createElement("tr");
+  tr.style.borderWidth = 0;
+
+  const th = document.createElement("th");
+  th.style.borderWidth = 0;
+  const text = document.createTextNode(`${ammo.category} - ${ammo.name}`);
+
+  th.appendChild(text);
+  tr.appendChild(th);
+  tableHead.appendChild(tr);
+
+  const tableBody = document.createElement("tbody");
+  const lines = [
+    {
+      key: "damage",
+      value: "Damage",
+    },
+    {
+      key: "armorDamage",
+      value: "Armor Damage",
+    },
+    {
+      key: "penValue",
+      value: "Penetration Value",
+    },
+    {
+      key: "fragChange",
+      value: "Frag. Chance",
+    },
+    {
+      key: "class1",
+      value: "Class 1",
+    },
+    {
+      key: "class2",
+      value: "Class 2",
+    },
+    {
+      key: "class3",
+      value: "Class 3",
+    },
+    {
+      key: "class4",
+      value: "Class 4",
+    },
+    {
+      key: "class5",
+      value: "Class 5",
+    },
+    {
+      key: "class6",
+      value: "Class 6",
+    },
+  ];
+
+  lines.forEach((line, i) => {
+    const color = line.key.includes("class")
+      ? getColor(ammo[line.key])
+      : "green";
+    const span = document.createElement("span");
+    span.style.background = color;
+    span.style.borderColor = color;
+    span.style.borderWidth = "2px";
+    span.style.marginRight = "10px";
+    span.style.height = "10px";
+    span.style.width = "10px";
+    span.style.display = "inline-block";
+
+    const tr = document.createElement("tr");
+    tr.style.backgroundColor = "inherit";
+    tr.style.borderWidth = 0;
+
+    const td = document.createElement("td");
+    td.style.borderWidth = 0;
+
+    console.log(ammo);
+    console.log(line.key);
+
+    const text = document.createTextNode(`${line.value}: ${ammo[line.key]}`);
+
+    td.appendChild(span);
+    td.appendChild(text);
+    tr.appendChild(td);
+    tableBody.appendChild(tr);
+  });
+
+  const tableRoot = tooltipEl.querySelector("table");
+
+  // Remove old children
+  while (tableRoot.firstChild) {
+    tableRoot.firstChild.remove();
+  }
+
+  // Add new children
+  tableRoot.appendChild(tableHead);
+  tableRoot.appendChild(tableBody);
+
+  const { offsetLeft: positionX, offsetTop: positionY } = chart.canvas;
+
+  // Display, position, and set styles for font
+  tooltipEl.style.opacity = 1;
+  tooltipEl.style.left = positionX + tooltip.caretX + "px";
+  tooltipEl.style.top = positionY + tooltip.caretY + "px";
+  tooltipEl.style.font = tooltip.options.bodyFont.string;
+  tooltipEl.style.padding =
+    tooltip.options.padding + "px " + tooltip.options.padding + "px";
+};
 
 const pointStyles = [
   {
@@ -24,11 +252,9 @@ const pointStyles = [
   },
   {
     pointStyle: "cross",
-    filled: true,
   },
   {
     pointStyle: "cross",
-    filled: true,
   },
   {
     pointStyle: "rect",
@@ -57,11 +283,9 @@ const pointStyles = [
   },
   {
     pointStyle: "star",
-    filled: true,
   },
   {
     pointStyle: "star",
-    filled: true,
   },
   {
     pointStyle: "triangle",
@@ -75,26 +299,24 @@ const pointStyles = [
     filled: true,
   },
   {
-    pointStyle: "retRot",
+    pointStyle: "rectRot",
   },
   {
-    pointStyle: "retRot",
+    pointStyle: "rectRot",
     filled: true,
   },
   {
-    pointStyle: "retRot",
+    pointStyle: "rectRot",
     filled: true,
+  },
+  {
+    pointStyle: "crossRot",
   },
   {
     pointStyle: "crossRot",
   },
   {
     pointStyle: "crossRot",
-    filled: true,
-  },
-  {
-    pointStyle: "crossRot",
-    filled: true,
   },
 ];
 
@@ -129,11 +351,13 @@ export default function AmmoChart({ results }) {
         return {
           x: ammo.damage,
           y: ammo.penValue,
-          name: ammo.name,
+          ...ammo,
+          category,
         };
       }),
       pointRadius: 8,
       pointHoverRadius: 12,
+      hidden: category !== "5.45x39 mm",
     };
 
     if (style.filled) {
@@ -151,8 +375,12 @@ export default function AmmoChart({ results }) {
       type: "scatter",
       data,
       options: {
+        maintainAspectRatio: false,
         responsive: true,
         plugins: {
+          annotation: {
+            annotations,
+          },
           datalabels: {
             align: "end",
             anchor: "end",
@@ -175,12 +403,15 @@ export default function AmmoChart({ results }) {
             },
           },
           tooltip: {
-            usePointStyle: true,
+            // usePointStyle: true,
             callbacks: {
               label: (context) => {
                 return context.raw.name;
               },
             },
+            enabled: false,
+            position: "nearest",
+            external: externalTooltipHandler,
           },
           legend: {
             position: "top",
@@ -276,19 +507,37 @@ export default function AmmoChart({ results }) {
             type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
             position: "left",
             ticks: {
-              color: "#ff00ff",
+              color: "white",
+            },
+            title: {
+              display: true,
+              text: "PENETRATION",
+              color: "white",
             },
           },
           y2: {
             type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
             position: "right",
             reverse: false,
-            ticks: {
-              color: "#ff00ff",
-            },
             grid: {
               drawOnChartArea: false, // only want the grid lines for one axis to show up
             },
+            max: 90,
+            min: 0,
+            display: false,
+          },
+          x: {
+            type: "linear",
+            title: {
+              display: true,
+              text: "DAMAGE",
+              color: "white",
+            },
+            ticks: {
+              color: "white",
+            },
+            suggestedMin: 30,
+            suggestedMin: 80,
           },
         },
       },
@@ -297,8 +546,10 @@ export default function AmmoChart({ results }) {
     myChart = new Chart(document.getElementById("line-chart"), config);
   }, []);
   return (
-    <Box>
-      <canvas id="line-chart" />
-    </Box>
+    <Center>
+      <Box w={["100%", "75%"]} mt="48px" h={["900px"]}>
+        <canvas id="line-chart" />
+      </Box>
+    </Center>
   );
 }
--- pages/nofoodaftermidnight.js
+++ pages/nofoodaftermidnight.js
@@ -8,8 +8,11 @@ import {
   Center,
   Text,
   Box,
-  Input,
-  Checkbox,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
 } from "@chakra-ui/react";
 import getResults from "../utils/getResults";
 import Legenda from "../components/Legenda";
@@ -22,7 +25,8 @@ import aRandomwordgeneratorperformsasimplebutusefultaskitgeneratesrandomwordsBut
 import { TwitchEmbed } from "react-twitch-embed";
 import CompareButton from "../components/CompareButton";
 import CompareModal from "../components/CompareModal";
-import AmmoChart from "../components/AmmoChart";
+import AmmoChart from "../components/Chart";
+import TableWrapper from "../components/TableWrapper";
 
 const App = ({ results, isFallback }) => {
   const [componentState, setComponentState] = useState({
@@ -37,34 +41,6 @@ const App = ({ results, isFallback }) => {
   console.log("isFallback", isFallback);
 
   const isMobile = useBreakpointValue({ base: true, md: false });
-  const keys = Object.keys(componentState.results);
-  let keysFilteredByWeaponName = keys;
-
-  if (componentState.currentSearch && componentState.currentSearch.length) {
-    keysFilteredByWeaponName = keys.filter((categoryName) =>
-      aRandomwordgeneratorperformsasimplebutusefultaskitgeneratesrandomwordsButwwwrandomwordgeneratororgdoesmorethanjustgeneraterandomwordsitletsyouchoosethenumberofwordsgeneratedsearchFilter(
-        componentState.currentSearch,
-        categoryName
-      )
-    );
-
-    keys.forEach((weaponName) => {
-      const weapon = componentState.results[weaponName];
-
-      weapon.forEach((ammo) => {
-        if (
-          aRandomwordgeneratorperformsasimplebutusefultaskitgeneratesrandomwordsButwwwrandomwordgeneratororgdoesmorethanjustgeneraterandomwordsitletsyouchoosethenumberofwordsgeneratedsearchFilter(
-            componentState.currentSearch,
-            ammo.name
-          )
-        ) {
-          if (!keysFilteredByWeaponName.includes(weaponName)) {
-            keysFilteredByWeaponName.push(weaponName);
-          }
-        }
-      });
-    });
-  }
 
   useEffect(() => {
     setComponentState({
@@ -83,114 +59,6 @@ const App = ({ results, isFallback }) => {
       ),
     });
   }, []);
-
-  const MobileTable = () => (
-    <>
-      <Accordion textAlign="center" defaultIndex={[0]} allowMultiple>
-        {keysFilteredByWeaponName.map((key, index) => {
-          const allAmmosForCategory = componentState.results[key];
-
-          return (
-            <Box
-              key={`allAmmos-${index}`}
-              color="#ebece8"
-              mx="8px"
-              mb="24px"
-              rounded="sm"
-              border="12px solid"
-              borderColor="vulcan.900"
-              bg="vulcan.900"
-            >
-              <AccordionItem>
-                <MobileRow
-                  category={key}
-                  allAmmosForCategory={allAmmosForCategory}
-                  currentSearch={componentState.currentSearch}
-                />
-              </AccordionItem>
-            </Box>
-          );
-        })}
-      </Accordion>
-    </>
-  );
-
-  const DesktopTable = () => (
-    <>
-      <Center>
-        <VStack
-          w={
-            componentState.minimalView
-              ? ["100%", "100%", "100%", "100%", "85%", "75%"]
-              : "100%"
-          }
-        >
-          {keysFilteredByWeaponName.map((key, index) => {
-            const allAmmosForCategory = componentState.results[key];
-
-            return (
-              <Box
-                key={`allAmmos-${index}`}
-                color="#ebece8"
-                mx="24px"
-                mb="24px"
-                rounded="sm"
-                border="12px solid"
-                borderColor="vulcan.900"
-                bg="vulcan.900"
-                w="100%"
-              >
-                <DesktopRow
-                  category={key}
-                  allAmmosForCategory={allAmmosForCategory}
-                  minimalView={componentState.minimalView}
-                  currentSearch={componentState.currentSearch}
-                  selectedAmmos={componentState.selectedAmmos}
-                  selectCallback={(ammo, newCheckboxValue) => {
-                    let newSelectedRows = [...componentState.selectedAmmos];
-
-                    if (newCheckboxValue) {
-                      if (
-                        !componentState.selectedAmmos.find(
-                          (row) =>
-                            row.name === ammo.name &&
-                            row.category === ammo.category
-                        )
-                      ) {
-                        newSelectedRows.push(ammo);
-                      }
-                    } else {
-                      const index = componentState.selectedAmmos.findIndex(
-                        (row) =>
-                          row.name === ammo.name &&
-                          row.category === ammo.category
-                      );
-
-                      if (index !== -1) {
-                        newSelectedRows = newSelectedRows
-                          .slice(0, index)
-                          .concat(
-                            newSelectedRows.slice(
-                              index + 1,
-                              newSelectedRows.length
-                            )
-                          );
-                      }
-                    }
-
-                    setComponentState({
-                      ...componentState,
-                      selectedAmmos: newSelectedRows,
-                    });
-                  }}
-                />
-              </Box>
-            );
-          })}
-        </VStack>
-      </Center>
-    </>
-  );
 
   return (
     <Box py="48px">
@@ -235,9 +103,6 @@ const App = ({ results, isFallback }) => {
           content="http://eft-ammo.com/assets/og-01.jpg"
         />
       </Head>
-      <Box h="100vh">
-        <AmmoChart results={results} />
-      </Box>
       {componentState.selectedAmmos.length && (
         <CompareButton
           showModal={() => {
@@ -341,45 +206,28 @@ const App = ({ results, isFallback }) => {
         </Flex>
       </Center>
 
-      <Center py="64px">
-        <Input
-          w={["100%", "50%"]}
-          mx="24px"
-          bg="#fff"
-          color="#333"
-          textAlign="center"
-          borderColor="#9a8866"
-          _focus={{ borderColor: "#9a8866" }}
-          placeholder="Search by Category or Ammo type"
-          _placeholder={{ color: "#333", textAlign: "center" }}
-          onChange={(e) => {
-            setComponentState({
-              ...componentState,
-              currentSearch: e.target.value,
-            });
-          }}
-        />
+      <Center>
+        <Tabs variant="solid-rounded" w="100%" mt="48px" colorScheme="purple">
+          <Center>
+            <TabList bg="vulcan.800" rounded="3xl">
+              <Tab color="white">Table View</Tab>
+              <Tab color="white">Chart View 🆕</Tab>
+            </TabList>
+          </Center>
+          <TabPanels>
+            <TabPanel>
+              <TableWrapper
+                isMobile={isMobile}
+                componentState={componentState}
+                setComponentState={setComponentState}
+              />
+            </TabPanel>
+            <TabPanel>
+              <AmmoChart results={results} />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Center>
-      {!isMobile && (
-        <Center>
-          <Checkbox
-            m="24px"
-            fontWeight="bold"
-            size="sm"
-            onChange={(e) =>
-              setComponentState({
-                ...componentState,
-                minimalView: !componentState.minimalView,
-              })
-            }
-            color={"#ebece8"}
-            isChecked={componentState.minimalView}
-          >
-            Minimal Table View
-          </Checkbox>
-        </Center>
-      )}
-      {isMobile ? <MobileTable /> : <DesktopTable />}
       <Center>
         <Box
           w={["375px", "450px", "600px"]}
@@ -427,7 +275,7 @@ export async function getStaticProps() {
   let isFallback = false;
 
   try {
-    results = await (await getResults()).json();
+    results = fallback;
   } catch (error) {
     results = fallback;
     isFallback = true;
