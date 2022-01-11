610e67c40d805ae22904c42ea90c879a0c28f92a
nicola genesin <nicolagenesin@yahoo.it>
2022-01-11 18:30:56 +0100

    Chart - Defeats buttons

--- components/Chart.js
+++ components/Chart.js
@@ -204,9 +204,6 @@ const externalTooltipHandler = (context) => {
     const td = document.createElement("td");
     td.style.borderWidth = 0;
 
-    console.log(ammo);
-    console.log(line.key);
-
     const text = document.createTextNode(`${line.value}: ${ammo[line.key]}`);
 
     td.appendChild(span);
@@ -336,221 +333,254 @@ const stringToColor = (str) => {
   return colour;
 };
 
-export default function AmmoChart({ results }) {
-  let myChart;
+let myChart;
 
-  const data = { datasets: [], labels: [] };
-
-  Object.keys(results).forEach((category, index) => {
-    const ammosForCategory = results[category];
-
-    data.labels.push(...ammosForCategory.map((ammo) => ammo.name));
-
-    const style = pointStyles[index];
-    const item = {
-      label: category,
-      pointStyle: style.pointStyle,
-      data: ammosForCategory.map((ammo) => {
-        return {
-          x: ammo.damage,
-          y: ammo.penValue,
-          ...ammo,
-          category,
-        };
-      }),
-      pointRadius: 8,
-      pointHoverRadius: 12,
-      hidden: category !== "5.45x39 mm",
-    };
-
-    if (style.filled) {
-      item.backgroundColor = stringToColor(category);
-    } else {
-      item.backgroundColor = "rgba(0,0,0,0)";
-      item.borderColor = stringToColor(category);
-    }
-
-    data.datasets.push(item);
-  });
-
+export default function AmmoChart({ results, classDefeated }) {
   React.useEffect(() => {
-    const config = {
-      type: "scatter",
-      data,
-      options: {
-        maintainAspectRatio: false,
-        responsive: true,
-        plugins: {
-          annotation: {
-            annotations,
-          },
-          datalabels: {
-            align: "end",
-            anchor: "end",
-            color: function (context) {
-              // if (context.dataset.borderColor) {
-              //   return context.dataset.borderColor;
-              // }
-              // return context.dataset.backgroundColor;
-              return "#ffffff";
+    const data = { datasets: [], labels: [] };
+
+    Object.keys(results).forEach((category, index) => {
+      const ammosForCategory = results[category];
+
+      data.labels.push(...ammosForCategory.map((ammo) => ammo.name));
+
+      const style = pointStyles[index];
+      const item = {
+        label: category,
+        pointStyle: style.pointStyle,
+        data: ammosForCategory
+          .filter((ammo) => {
+            if (classDefeated) {
+              if (classDefeated * 10 > ammo.penValue) {
+                return false;
+              } else {
+                return true;
+              }
+            } else {
+              return true;
+            }
+          })
+          .map((ammo) => {
+            return {
+              x: ammo.damage,
+              y: ammo.penValue,
+              ...ammo,
+              category,
+            };
+          }),
+        pointRadius: 8,
+        pointHoverRadius: 12,
+      };
+
+      if (!myChart) {
+        item.hidden = category !== "5.45x39 mm";
+      }
+
+      if (style.filled) {
+        item.backgroundColor = stringToColor(category);
+      } else {
+        item.backgroundColor = "rgba(0,0,0,0)";
+        item.borderColor = stringToColor(category);
+      }
+
+      data.datasets.push(item);
+    });
+
+    if (myChart) {
+      const hiddenDatasets = [];
+      for (let i = 0; i < myChart.data.datasets.length; i++) {
+        if (!myChart.isDatasetVisible(i)) {
+          hiddenDatasets.push(myChart.data.datasets[i]);
+        }
+      }
+
+      data.datasets.forEach((newSet, index) => {
+        if (hiddenDatasets.find((hidden) => hidden.label === newSet.label)) {
+          newSet.hidden = true;
+        }
+      });
+
+      myChart.data.datasets = data.datasets;
+      myChart.update("none");
+    } else {
+      const config = {
+        type: "scatter",
+        data,
+        options: {
+          maintainAspectRatio: false,
+          responsive: true,
+          plugins: {
+            annotation: {
+              annotations,
             },
-            font: function (context) {
-              var w = context.chart.width;
-              return {
-                size: w < 512 ? 12 : 14,
-                // weight: "bold",
-              };
-            },
-            formatter: function (value, context) {
-              return value.name;
-            },
-          },
-          tooltip: {
-            // usePointStyle: true,
-            callbacks: {
-              label: (context) => {
-                return context.raw.name;
+            datalabels: {
+              align: "end",
+              anchor: "end",
+              color: function (context) {
+                // if (context.dataset.borderColor) {
+                //   return context.dataset.borderColor;
+                // }
+                // return context.dataset.backgroundColor;
+                return "#ffffff";
+              },
+              font: function (context) {
+                var w = context.chart.width;
+                return {
+                  size: w < 512 ? 12 : 14,
+                  // weight: "bold",
+                };
+              },
+              formatter: function (value, context) {
+                return value.name;
               },
             },
-            enabled: false,
-            position: "nearest",
-            external: externalTooltipHandler,
-          },
-          legend: {
-            position: "top",
-            labels: {
-              padding: 20,
-              usePointStyle: true,
-              generateLabels: (chart) => {
-                const datasets = chart.data.datasets;
-                const {
-                  labels: { usePointStyle, pointStyle, textAlign, color },
-                } = chart.legend.options;
+            tooltip: {
+              // usePointStyle: true,
+              callbacks: {
+                label: (context) => {
+                  return context.raw.name;
+                },
+              },
+              enabled: false,
+              position: "nearest",
+              external: externalTooltipHandler,
+            },
+            legend: {
+              position: "top",
+              labels: {
+                padding: 20,
+                usePointStyle: true,
+                generateLabels: (chart) => {
+                  const datasets = chart.data.datasets;
+                  const {
+                    labels: { usePointStyle, pointStyle, textAlign, color },
+                  } = chart.legend.options;
 
-                const legendItems = chart
-                  ._getSortedDatasetMetas()
-                  .map((meta) => {
-                    const style = meta.controller.getStyle(
-                      usePointStyle ? 0 : undefined
-                    );
+                  const legendItems = chart
+                    ._getSortedDatasetMetas()
+                    .map((meta) => {
+                      const style = meta.controller.getStyle(
+                        usePointStyle ? 0 : undefined
+                      );
 
-                    return {
-                      text: datasets[meta.index].label,
-                      fillStyle: style.backgroundColor,
-                      fontColor: color,
-                      hidden: !meta.visible,
-                      lineCap: style.borderCapStyle,
-                      lineDash: style.borderDash,
-                      lineDashOffset: style.borderDashOffset,
-                      lineJoin: style.borderJoinStyle,
-                      strokeStyle: style.borderColor,
-                      pointStyle: pointStyle || style.pointStyle,
-                      rotation: style.rotation,
-                      textAlign: textAlign || style.textAlign,
-                      datasetIndex: meta.index,
-                    };
+                      return {
+                        text: datasets[meta.index].label,
+                        fillStyle: style.backgroundColor,
+                        fontColor: color,
+                        hidden: !meta.visible,
+                        lineCap: style.borderCapStyle,
+                        lineDash: style.borderDash,
+                        lineDashOffset: style.borderDashOffset,
+                        lineJoin: style.borderJoinStyle,
+                        strokeStyle: style.borderColor,
+                        pointStyle: pointStyle || style.pointStyle,
+                        rotation: style.rotation,
+                        textAlign: textAlign || style.textAlign,
+                        datasetIndex: meta.index,
+                      };
+                    });
+
+                  legendItems.push({
+                    text:
+                      !chart.legend.hideAll ||
+                      typeof chart.legend.hideAll === "undefined"
+                        ? "Hide All"
+                        : "Show All",
+                    fontColor: color,
+                    fillStyle: "turquoise", // Box color
+                    strokeStyle: "turquoise", // LineCollor around box
                   });
 
-                legendItems.push({
-                  text:
-                    !chart.legend.hideAll ||
-                    typeof chart.legend.hideAll === "undefined"
-                      ? "Hide All"
-                      : "Show All",
-                  fontColor: color,
-                  fillStyle: "turquoise", // Box color
-                  strokeStyle: "turquoise", // LineCollor around box
-                });
-
-                return legendItems;
+                  return legendItems;
+                },
               },
-            },
-            onClick: (evt, legendItem, legend) => {
-              const type = legend.chart.config.type;
-              let allLegendItemsState = [];
+              onClick: (evt, legendItem, legend) => {
+                const type = legend.chart.config.type;
+                let allLegendItemsState = [];
 
-              if (
-                legendItem.text === "Hide All" ||
-                legendItem.text === "Show All"
-              ) {
-                if (typeof legend.hideAll === "undefined") {
-                  legend.hideAll = false;
+                if (
+                  legendItem.text === "Hide All" ||
+                  legendItem.text === "Show All"
+                ) {
+                  if (typeof legend.hideAll === "undefined") {
+                    legend.hideAll = false;
+                  }
+
+                  legend.chart.data.datasets.forEach((dataset, i) => {
+                    legend.chart.setDatasetVisibility(i, legend.hideAll);
+                  });
+
+                  legend.hideAll = !legend.hideAll;
+                  legend.chart.update();
+
+                  return;
                 }
 
-                legend.chart.data.datasets.forEach((dataset, i) => {
-                  legend.chart.setDatasetVisibility(i, legend.hideAll);
-                });
+                defaultLegendClickHandler(evt, legendItem, legend);
 
-                legend.hideAll = !legend.hideAll;
-                legend.chart.update();
+                allLegendItemsState = legend.chart.data.datasets.map(
+                  (e, i) => legend.chart.getDatasetMeta(i).hidden
+                );
 
-                return;
-              }
-
-              defaultLegendClickHandler(evt, legendItem, legend);
-
-              allLegendItemsState = legend.chart.data.datasets.map(
-                (e, i) => legend.chart.getDatasetMeta(i).hidden
-              );
-
-              if (allLegendItemsState.every((el) => !el)) {
-                legend.hideAll = false;
-                legend.chart.update();
-              } else if (allLegendItemsState.every((el) => el)) {
-                legend.hideAll = true;
-                legend.chart.update();
-              }
-            },
-          },
-          title: {
-            display: false,
-          },
-        },
-        scales: {
-          y: {
-            type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-            position: "left",
-            ticks: {
-              color: "white",
+                if (allLegendItemsState.every((el) => !el)) {
+                  legend.hideAll = false;
+                  legend.chart.update();
+                } else if (allLegendItemsState.every((el) => el)) {
+                  legend.hideAll = true;
+                  legend.chart.update();
+                }
+              },
             },
             title: {
-              display: true,
-              text: "PENETRATION",
-              color: "white",
+              display: false,
             },
-            max: 90,
-            min: 0,
           },
-          y2: {
-            type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-            position: "right",
-            reverse: false,
-            grid: {
-              drawOnChartArea: false, // only want the grid lines for one axis to show up
+          scales: {
+            y: {
+              type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+              position: "left",
+              ticks: {
+                color: "white",
+              },
+              title: {
+                display: true,
+                text: "PENETRATION",
+                color: "white",
+              },
+              max: 90,
+              min: 0,
             },
-            max: 90,
-            min: 0,
-            display: false,
-          },
-          x: {
-            type: "linear",
-            title: {
-              display: true,
-              text: "DAMAGE",
-              color: "white",
+            y2: {
+              type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+              position: "right",
+              reverse: false,
+              grid: {
+                drawOnChartArea: false, // only want the grid lines for one axis to show up
+              },
+              max: 90,
+              min: 0,
+              display: false,
             },
-            ticks: {
-              color: "white",
+            x: {
+              type: "linear",
+              title: {
+                display: true,
+                text: "DAMAGE",
+                color: "white",
+              },
+              ticks: {
+                color: "white",
+              },
+              suggestedMin: 30,
+              suggestedMin: 80,
             },
-            suggestedMin: 30,
-            suggestedMin: 80,
           },
         },
-      },
-    };
+      };
 
-    myChart = new Chart(document.getElementById("line-chart"), config);
-  }, []);
+      myChart = new Chart(document.getElementById("line-chart"), config);
+    }
+  }, [classDefeated]);
   return (
     <Center>
       <Box w={["100%", "75%"]} mt="48px" h={["900px"]}>
--- components/ChartWrapper.js
+++ components/ChartWrapper.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Chart from "../components/Chart";
+import { Button, Center, Wrap, WrapItem } from "@chakra-ui/react";
 
 const ChartWrapper = ({ results }) => {
+  const [state, setState] = useState({
+    classDefeated: undefined,
+  });
+
   return (
     <>
-      <Chart results={results} />
+      <Chart results={results} classDefeated={state.classDefeated} />
+      <Center mt="16px">
+        <Wrap justify="center">
+          {[...Array(7)]
+            .map((_, i) => 0 + i)
+            .map((index) => {
+              return (
+                <WrapItem>
+                  <Button
+                    size={"sm"}
+                    colorScheme={index === 0 ? "red" : "purple"}
+                    onClick={() => {
+                      setState({
+                        ...state,
+                        classDefeated: index,
+                      });
+                    }}
+                  >
+                    {index === 0 ? "Reset" : `Defeats CLASS ${index}`}
+                  </Button>
+                </WrapItem>
+              );
+            })}
+        </Wrap>
+      </Center>
     </>
   );
 };
