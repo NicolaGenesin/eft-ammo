5b64b2d36771f7693e45f424894632105dbbea16
nicola genesin <nicolagenesin@yahoo.it>
2022-01-21 13:46:09 +0100

    Point to test domain

--- utils/getResults.js
+++ utils/getResults.js
@@ -1,5 +1,7 @@
 const dev = process.env.NEXT_NODE_ENV !== "production";
-const url = dev ? "http://localhost:3000" : "https://eft-ammo.com";
+const url = dev
+  ? "http://localhost:3000"
+  : "https://eft-ammo-git-nofam-data-v2-1911z.vercel.app";
 
 const getResults = async () => {
   const results = fetch(`${url}/api/data`, {
