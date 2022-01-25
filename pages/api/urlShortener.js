a8bca67635f54a9c29254a9b3fc9d741fb14fdc0
nicola genesin <nicolagenesin@yahoo.it>
2022-01-25 15:41:36 +0100

    Write shortened URLs to GS

--- pages/api/urlShortener.js
+++ pages/api/urlShortener.js
@@ -1,9 +1,32 @@
 import { withSentry } from "@sentry/nextjs";
 const tinyURL = require("tinyurl");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+
+const writeToGoogleSheet = async (link) => {
+  const doc = new GoogleSpreadsheet(
+    "18QS7LFyM-Hy5xgFqVAPHeJKhAdUUiL6ZFU1eXGn5WHQ"
+  );
+  await doc.useServiceAccountAuth({
+    client_email: process.env.NEXT_GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.NEXT_GOOGLE_PRIVATE_KEY,
+  });
+  await doc.loadInfo(); // loads document properties and worksheets
+
+  const sheet = doc.sheetsByIndex[0];
+
+  await sheet.loadHeaderRow(1);
+  await sheet.addRow([link]);
+};
 
 const handler = async (req, res) => {
   const body = req.body;
   const result = await tinyURL.shorten(body.link);
+
+  try {
+    await writeToGoogleSheet(result);
+  } catch (error) {
+    console.log("Error while saving to spreadsheet");
+  }
 
   res.status(200).json({ result });
 };
