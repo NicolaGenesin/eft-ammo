4ef5d97c44facd4bea1869b925dc5d60f990c5f0
nicola genesin <nicolagenesin@yahoo.it>
2022-02-03 11:47:02 +0100

    Update dev value

--- utils/env.js
+++ utils/env.js
@@ -1,2 +1,2 @@
-export const dev = process.env.NEXT_NODE_ENV !== "production";
+export const dev = process.env.NODE_ENV === "development";
 export const url = dev ? "http://localhost:3000" : "https://eft-ammo.com";
