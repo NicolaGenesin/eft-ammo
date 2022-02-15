d0246920b00cc2e8d4d7d40d0bbe9a665c8ea657
nicola genesin <nicolagenesin@yahoo.it>
2022-02-15 11:37:40 +0100

    TEMP change backend url

--- utils/env.js
+++ utils/env.js
@@ -1,2 +1,4 @@
 export const dev = process.env.NODE_ENV === "development";
-export const url = dev ? "http://localhost:3000" : "https://eft-ammo.com";
+export const url = dev
+  ? "http://localhost:3000"
+  : "https://eft-ammo-git-gun-builder-feature-1911z.vercel.app";
