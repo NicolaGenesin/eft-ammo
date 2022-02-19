ebbb3577298f64d2e4351d0fc7c9813b2fc41938
nicola genesin <nicolagenesin@yahoo.it>
2022-02-19 01:19:55 +0100

    Go easier on api

--- pages/api/guns/builds/builds.js
+++ pages/api/guns/builds/builds.js
@@ -6,10 +6,10 @@ const handler = async (req, res) => {
 
   switch (method) {
     case "GET":
-      let queryToExec = `select value from build a join configuration b on a.code = b.code where value is not null and json_length(value) > 1 order by createdAt desc limit 150`;
+      let queryToExec = `select value from build a join configuration b on a.code = b.code where value is not null and json_length(value) > 1 order by createdAt desc limit 20`;
 
       if (query.orderBy === "score") {
-        queryToExec = `select value from build a join configuration b on a.code = b.code where value is not null and json_length(value) > 1 order by createdAt desc limit 150`;
+        queryToExec = `select value from build a join configuration b on a.code = b.code where value is not null and json_length(value) > 1 order by createdAt desc limit 20`;
       }
 
       const [getRows, _] = await conn.query(queryToExec);
